fad18ecfef92b6adb4b1ab9f11e8fda72d7683bf
anthonyjk <navyseals1405@gmail.com>
2024-10-31 16:31:36 -0400

    Created base ecosystem file

---
+++ ecosystem.py
@@ -0,0 +1,11 @@
+# Ecosystem model
+import numpy as np
+
+creatures = {"baleen whale":25,
+             "krill":1000000,
+             "leopard seal": 25,
+             "arctic cod": 10000,
+             "penguin": 750,
+             "orca":10,
+             "plankton": np.inf # Infinite Plankton
+}
