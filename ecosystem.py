2ec0fdecdab2dd220e390ad97537936430c4a7e6
anthonyjk <navyseals1405@gmail.com>
2024-10-31 16:38:15 -0400

    stub functions

--- ecosystem.py
+++ ecosystem.py
@@ -9,3 +9,27 @@ creatures = {"baleen whale":25,
              "orca":10,
              "plankton": np.inf # Infinite Plankton
 }
+
+def krill():
+    # https://marinesanctuary.org/blog/krill-behavior-diet-lifecycle/
+    pass
+    # increase pop number by random amount from source
+
+def arctic_cod():
+    #
+    pass
+    # increase pop number by random amount
+    # decrease krill pop by related number
+    # ex: growth of 25, kills 10,000 krill
+
+def leopard_seal():
+    pass
+
+def penguin():
+    pass
+
+def orca():
+    pass
+
+def baleen_whale():
+    pass
